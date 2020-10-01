fbb3fac546741ca39fb63fbbae0f3a2b572dc86d
Demin Alex <fizmail@inbox.ru>
2020-10-01 22:23:01 +0300

    Update lect5.cpp

--- lect5.cpp
+++ lect5.cpp
@@ -29,11 +29,22 @@ class TheCollection { // На динамическом массиве
 
     // Или так (лучше). 
   private:
-    TheData *objects;// Массив объектов
+    TheData *objects; // Массив объектов
     int count; // Число объектов.
     int allocated; // Выделено объектов.
     // Выделять, освобождать память долго.
     // Резервируем дополнительную память на  несколько (а не один) объектов.
     void grow(); // Выделяем здесь.
 };
+
+
+// Выберите одно решение из двух.
+TheCollection::TheCollection() {
+    objects=NULL; 
+    count=allocated=0;
+}
+
+// В стиле С++98 можно так:
+TheCollection::TheCollection() :objects(NULL),count(0),allocated(0) {
+}
 
