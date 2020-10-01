ffb518997076845f7f8dc5db014dfc0cedfbddfe
Demin Alex <fizmail@inbox.ru>
2020-10-01 22:02:42 +0300

    Update lect5.cpp

--- lect5.cpp
+++ lect5.cpp
@@ -13,3 +13,27 @@ class TheCollection { // На динамическом массиве
     TheData *objects;// Массив объектов
     int count; // Число объектов.
 };
+
+
+class TheCollection { // На динамическом массиве
+  public:  
+    TheCollection();
+    TheCollection(const TheCollection&);
+    TheCollection(const TheCollection&&);
+    ~TheCollection();
+    void add(const TheData &obj);
+    void del(int pos);
+    void del(const TheData &obj);
+    TheData &get(int pos);
+    void set(int pos, const TheData&);
+
+    // Или так (лучше). 
+  private:
+    TheData *objects;// Массив объектов
+    int count; // Число объектов.
+    int allocated; // Выделено объектов.
+    // Выделять, освобождать память долго.
+    // Резервируем дополнительную память на  несколько (а не один) объектов.
+    void grow(); // Выделяем здесь.
+};
+
